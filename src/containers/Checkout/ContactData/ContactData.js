2251027ec8785d951951dfc2af4bb01e69e1bd92
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-04-20 00:14:30 +0300

    Create ContectData component

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+import classes from './ContactData.css';
+import Button from '../../../components/UI/Button/Button';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    }
+
+  }
+
+  render () {
+    return (
+      <div className={classes.CntactData}>
+        <h4>Enter your data</h4>
+        <form>
+          <input type="text" name="name" placeholder="Your name" />
+          <input type="email" name="email" placeholder="Your email" />
+          <input type="text" name="street" placeholder="Street" />
+          <input type="text" name="postal" placeholder="Postal Code" />
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ContactData;
