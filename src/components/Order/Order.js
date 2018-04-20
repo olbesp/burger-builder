5aa42fe300dc6d145da2450b0847566cf1b6b44a
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-04-20 15:42:07 +0300

    Add Orders page route

---
+++ src/components/Order/Order.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => (
+  <div className={classes.Order}>
+    <p>Ingrediants: </p>
+  </div>
+);
+
+export default order;
