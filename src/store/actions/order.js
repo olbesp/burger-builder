9698d2163d96baba8ee98af26f8a8049e3970bd2
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-17 12:22:11 +0300

    Create order reducer

---
+++ src/store/actions/order.js
@@ -0,0 +1,28 @@
+import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => ({
+  type: actionTypes.PURCHASE_BURGER_SUCCESS,
+  orderID: id,
+  orderData
+});
+
+export const purchaseBurgerFail = error => ({
+  type: actionTypes.PURCHASE_BURGER_FAIL,
+  error
+});
+
+export const purchaseBurgerStart = () => ({
+  type: actionTypes.PURCHASE_BURGER_START
+});
+
+export const purchaseBurger = orderData => dispatch => {
+  dispatch(purchaseBurgerStart());
+  axios.post('/orders.json', orderData)
+    .then(response => {
+      dispatch(purchaseBurgerSuccess(response.data, orderData));
+    })
+    .catch(error => {
+      dispatch(purchaseBurgerFail(error));
+    });
+};
