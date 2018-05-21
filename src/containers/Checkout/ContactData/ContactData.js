bedf349a4d5c0f06df70535342b5b45b0f4d67f9
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-21 15:01:04 +0300

    Make orders page protected

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -107,7 +107,7 @@ class ContactData extends Component {
             price: this.props.price,
             orderData: formData
         };
-        this.props.onOrderBurger(order);
+        this.props.onOrderBurger(order, this.props.token);
     }
 
     checkValidity(value, rules) {
@@ -199,11 +199,12 @@ class ContactData extends Component {
 const mapStateToProps = state => ({
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    loading: state.order.loading
+    loading: state.order.loading,
+    token: state.auth.token
 });
 
 const mapDispatchToProps = dispatch => ({
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+    onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -16,9 +16,9 @@ export const purchaseBurgerStart = () => ({
   type: actionTypes.PURCHASE_BURGER_START
 });
 
-export const purchaseBurger = orderData => dispatch => {
+export const purchaseBurger = (orderData, token) => dispatch => {
   dispatch(purchaseBurgerStart());
-  axios.post('/orders.json', orderData)
+  axios.post('/orders.json?auth=' + token, orderData)
     .then(response => {
       dispatch(purchaseBurgerSuccess(response.data.name, orderData));
     })
@@ -45,9 +45,9 @@ export const fetchOrdersStart = () => ({
   type: actionTypes.FETCH_ORDERS_START
 });
 
-export const fetchOrders = () => dispatch => {
+export const fetchOrders = token => dispatch => {
   dispatch(fetchOrdersStart());
-  axios.get('/orders.json')
+  axios.get('/orders.json?auth=' + token)
     .then(res => {
       const fetchedOrders = [];
       for (let key in res.data) {
