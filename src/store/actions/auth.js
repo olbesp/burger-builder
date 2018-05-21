c02a02df7ef99659d9bd846a0bc43d9a703c4975
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-21 11:18:45 +0300

    Add signing up with firebase

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => ({
   type: actionTypes.AUTH_START
@@ -16,4 +17,18 @@ export const authFail = error => ({
 
 export const auth = (email, password) => dispatch => {
   dispatch(authStart());
+  const authData = {
+    email,
+    password,
+    returnSecureToken: true
+  };
+  axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyD3Cj_jyAuY58UUtTsiC5aE5LwCjNwC3jo', authData)
+    .then(response => {
+      console.log(response);
+      dispatch(authSuccess(response.data));
+    })
+    .catch(error => {
+      console.log(error);
+      dispatch(authFail(error));
+    });
 };
