39c6bfcf6290536fbc88b3d331d528390949ba52
Oleksii Bespalko <ol.bespalko@gmail.com>
2018-05-22 11:04:25 +0300

    Redirect user to the checkout page

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -5,7 +5,8 @@ const initialState = {
   token: null,
   userID: null,
   error: null,
-  loading: false
+  loading: false,
+  authRedirectPath: '/'
 };
 
 const authStart = (state, action) => updateObject(state, { error: null, loading: true });
@@ -24,12 +25,15 @@ const authFail = (state, action) => updateObject(state, {
 
 const authLogout = (state, action) => updateObject(state, { token: null, userID: null });
 
+const setAuthRedirectPath = (state, action) => updateObject(state, { authRedirectPath: action.path });
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.AUTH_START: return authStart(state, action);
     case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
     case actionTypes.AUTH_FAIL: return authFail(state, action);
     case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
+    case actionTypes.SET_AUTH_REDIRECT_PATH: return setAuthRedirectPath(state, action);
     default: return state;
   }
 };
--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -4,7 +4,8 @@ import { updateObject } from '../utility';
 const initialState = {
   ingredients: null,
   totalPrice: 4,
-  error: false
+  error: false,
+  building: false
 };
 
 const INGREDIENT_PRICES = {
@@ -19,7 +20,8 @@ const addIngredient = (state, action) => {
   const addedIngredients = updateObject(state.ingredients, addedIngredient);
   const addedState = {
     ingredients: addedIngredients,
-    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+    building: true
   };
   return updateObject(state, addedState);
 };
@@ -29,7 +31,8 @@ const removeIngredient = (state,action) => {
   const removedIngredients = updateObject(state.ingredients, removedIngredient);
   const removedState = {
     ingredients: removedIngredients,
-    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
+    building: true
   };
   return updateObject(state, removedState);
 };
@@ -38,7 +41,8 @@ const setIngredients = (state, action) => (
   updateObject(state, {
     ingredients: action.ingredients,
     totalPrice: 4,
-    error: false
+    error: false,
+    building: false
   })
 );
 
